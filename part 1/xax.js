7b507089d50be6bb7c1c5a45050a09fafe31f73e
GarushMarkosyan <garush.markosyan@tumo.org>
2023-07-19 17:58:06 +0400

    avelacrel em nor nkarner

--- part 1/xax.js
+++ part 1/xax.js
@@ -3,4 +3,117 @@ const elements = [
         "logo": "logoner ca.svg",
         "name": "pubg mobile"
     },
+    {
+        "logo": "logoner ca.svg",
+        "name": "among us"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "antistress"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "brawl"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "car parking"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "clash of clansh"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "fifa"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "free fire"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "geometry dash"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "granny"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "hide online"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "gta"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    {
+        "logo": "logoner ca.svg",
+        "name": "pubg mobile"
+    },
+    
 ]
